695043253e389d3c128c47dd3fdba977af324110
hakimfrh <hakimfrh145@gmail.com>
2024-05-15 01:07:24 +0700

    add listview builder

--- lib/user_list.dart
+++ lib/user_list.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:myevent/database/api.dart';
 import 'package:http/http.dart' as http;
+import 'package:myevent/event/card_event.dart';
+import 'package:myevent/model/event.dart';
 
 class UserList extends StatefulWidget {
   const UserList({super.key});
@@ -17,6 +19,44 @@ final List<String> categories = [
   'Ditolak',
   'Menunggu Pembayaran',
   'Selesai'
+];
+
+List<Eventt> eventList = [
+  Eventt(
+      idEvent: '123123',
+      status: 'Selesai',
+      tanggal: '28 Februari 2023',
+      namaEvent: 'Sunday Services',
+      deskripsiEvent:
+          'Festival yang menggabungkan seni, musik, dan aktivitas hijau untuk mempromosikan kesadaran lingkungan dan kreativitas.',
+      hargaMin: '250.000',
+      hargaMax: '500.000',
+      lokasi: 'jember',
+      noWa: '12308128123',
+      cover: 'logo.png'),
+  Eventt(
+      idEvent: '456456',
+      status: 'Menunggu Pembayaran',
+      tanggal: '40 Februari 2023',
+      namaEvent: 'Summer Van Tour',
+      deskripsiEvent:
+          'Konser hura hura bersama artis terkenal. Dimeriahkan oleh Coldplay, Ed-Shetan, dan Puyung Teduh.',
+      hargaMin: '300.000',
+      hargaMax: '800.000',
+      lokasi: 'Jakarta',
+      noWa: '12308120123',
+      cover: 'logo.png'),
+  Eventt(
+      idEvent: '789789',
+      status: 'Ditolak',
+      tanggal: '10 Februari 2023',
+      namaEvent: 'Black Parade',
+      deskripsiEvent: 'Ajang festival peringatan hari hitam sedunia',
+      hargaMin: '300.000',
+      hargaMax: '800.000',
+      lokasi: 'Malang',
+      noWa: '12308120123',
+      cover: 'logo.png'),
 ];
 
 class _UserListState extends State<UserList> {
@@ -59,6 +99,15 @@ class _UserListState extends State<UserList> {
               }),
             ),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: eventList.length, // Jumlah total item dalam daftar
+              itemBuilder: (context, index) {
+                Eventt event = eventList[index];
+                return CardEvent(event: event);
+              },
+            ),
+          )
         ],
       ),
     );
