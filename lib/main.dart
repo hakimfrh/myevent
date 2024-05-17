5522a8117d17f22a175cdf43b7e23d1c4a37a091
Vendri14 <124660160+Vendri14@users.noreply.github.com>
2024-05-17 23:17:42 +0700

    Pembayaran
    Pembayaran

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter GetX Routing Example',
       debugShowCheckedModeBanner: false,
       // Define initial route
-      initialRoute: '/dashboard',
+      initialRoute: '/pembayaran',
       // Define routes
       getPages: [
         // Example of a named route
